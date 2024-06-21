cc17e5ca407d311a022aca5346a262a95cb32584
Hau'olinalani O'Brien <ulapulelehua@gmail.com>
2024-06-21 13:09:46 -1000

    ok insanity has started

---
+++ proofity/ND/ND-RULES/NDEnterRaa.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Proofity
+{
+    public class NDEnterRaa : NDRule
+    {
+        public static string _name = "enter_raa";
+        public override string Name => _name;
+
+        public sealed override string Label { get; set; }
+        public sealed override AST Predicate { get; set; }
+        public sealed override List<string> Requirements { get; set; }
+
+        public NDEnterRaa(string label, AST predicate, List<string> requirements)
+        {
+            Label = label;
+            Requirements = requirements;
+            Predicate = predicate;
+        }
+
+        public override bool CheckRule(SymbolTable symbolTable, List<AST> premises)
+        {
+            return true;
+        }
+    }
+}
