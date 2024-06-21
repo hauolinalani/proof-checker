e9a678d4c127c66d31a0f3990f644f037db46707
Hau'olinalani O'Brien <ulapulelehua@gmail.com>
2024-06-21 13:07:22 -1000

    devious work

---
+++ proofity/ND/ND-RULES/NDEnterForalll.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace Proofity
+{
+    public class NDEnterForallI : NDRule
+    {
+        public static string _name = "enter_forall_i";
+        public override string Name => _name;
+
+        public sealed override string Label { get; set; }
+        public sealed override AST Predicate { get; set; }
+        public sealed override List<string> Requirements { get; set; }
+
+        public NDEnterForallI(string label, AST predicate, List<string> requirements)
+        {
+            Label = label;
+            Requirements = requirements;
+            Predicate = predicate;
+        }
+
+        public override bool CheckRule(SymbolTable symbolTable, List<AST> premises)
+        {
+            return true;
+        }
+    }
+}
